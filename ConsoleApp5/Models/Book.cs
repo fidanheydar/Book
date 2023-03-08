e30aca6a5b0caef59c52ef5b3eb48c63bb7f30e5
fidannh <fidannh@code.edu.az>
2023-03-09 03:07:22 +0400

    task

---
+++ ConsoleApp5/Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5.Models
+{
+    internal class Book
+    {
+        public string Name { get; set; }
+        public int PageCount { get; set; }  
+      
+
+        public Book(string name, int page)
+        {
+           this.Name = name;
+            this.PageCount = page;
+         
+        }
+    }
+}
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,74 @@
+﻿
+
+using ConsoleApp5.Models;
+using System.Xml.Linq;
+
+Console.WriteLine();
+
+
+
+Book[] Books = new Book[10];
+for(int i = 0; i < Books.Length; i++)
+{
+
+    Console.WriteLine("Enter the Name:");
+    string Name = Console.ReadLine();
+    Console.WriteLine("Enter the PageCount:");
+    int page = int.Parse(Console.ReadLine());
+
+    while (!CheckName(Name))
+    {
+        Console.WriteLine("Enter correct name");
+        Name= Console.ReadLine();
+    }
+    while (!CheckName(page))
+    {
+        Console.WriteLine("Enter correct pagecount");
+        int Page = int.Parse(Console.ReadLine());
+    }
+    while (Search(Name, PageCount))
+    {
+        Console.WriteLine("This book is already exsists  ");
+        Console.WriteLine("Enter the correct name");
+        Name = Console.ReadLine();
+        Console.WriteLine("Enter the correct page count");
+        PageCount = int.Parse(Console.ReadLine());
+    }
+
+    Book book = new Book(Name,page);
+    Books[i] = book;
+
+
+    bool CheckName(string name)
+    {
+        if (!string.IsNullOrWhiteSpace(name) && name.Length > 10) ;
+        return true;
+
+        return false;
+    }
+
+
+
+    bool CheckName(int pageCount)
+    {
+        if(pageCount > 10)
+            return true;
+
+        return false;
+    }
+}
+
+bool CheckName(int page)
+{
+    throw new NotImplementedException();
+}
+
+bool Search(object bookName)
+{
+    throw new NotImplementedException();
+}
+
+foreach (Book book in Books)
+{
+ Console.WriteLine($"Name : {book.Name}  pageCount :{book.PageCount}");
+}
